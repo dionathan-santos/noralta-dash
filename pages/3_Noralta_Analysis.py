367929864baeb5b9d02cb17d0401fd449f59f5b5
Dionathan <adiel8@gmail.com>
2025-01-16 15:41:18 -0700

    update

--- pages/3_Noralta_Analysis.py
+++ pages/3_Noralta_Analysis.py
@@ -123,7 +123,7 @@ def main():
         (data['Year Built'].isin(selected_years) if selected_years else True) &
         (data['Property Class'].isin(selected_property_types) if selected_property_types else True) &
         (data['Building Type'].isin(selected_building_types) if selected_building_types else True) &
-        (data['Community'].isin(selected_communities) if selected_communities else True)
+        (data['Community'].isin(selected_communities) if selected_communities else True) &
         (data['Area/City'].isin(selected_cities) if selected_cities else True)
 )
 
