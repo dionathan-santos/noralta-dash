f6a5eae62ea246b92abeee0580fb3c2a46c990ef
Dionathan <adiel8@gmail.com>
2025-01-17 12:04:16 -0700

    update

--- pages/2_Market_Analysis.py
+++ pages/2_Market_Analysis.py
@@ -10,7 +10,10 @@ import numpy as np
 
 
 def main():
-    st.title("Real Estate Market Analysis Dashboard")
+    st.set_page_config(
+        st.title("Real Estate Market Analysis Dashboard")
+                layout="wide"  # Ensures wide mode
+)
 
 def get_mongodb_data(mongodb_uri, database_name, collection_name):
     """
