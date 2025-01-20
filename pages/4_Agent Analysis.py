fdb7ce842f444233d32d849d65d243fd11246f8d
Dionathan <adiel8@gmail.com>
2025-01-20 10:49:05 -0700

    update pag 5

--- pages/4_Agent Analysis.py
+++ pages/4_Agent Analysis.py
@@ -145,7 +145,7 @@ else:
     firm_info = f"Firms: {', '.join(all_firms)}"
 
 # Display the firm(s) information below the KPIs
-st.write.metric(firm_info)
+st.markdown(:red[firm_info])
 
 # Group by month and calculate deals
 filtered_data['Month'] = filtered_data['Sold Date'].dt.to_period('M').dt.to_timestamp()
