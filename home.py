3ed180288530c9a7714ec67afaf63a681b19b9dc
Dionathan <adiel8@gmail.com>
2025-01-30 13:07:31 -0700

    test6

--- home.py
+++ home.py
@@ -9,3 +9,8 @@ st.set_page_config(
 
 st.title("Welcome to the Noralta Dashboard!")
 st.write("Use the sidebar to navigate through the different pages of the dashboard.")
+
+
+####### Create a content index 
+####### Add best agent in the region within best firm
+####### Fix the poor perfomance agents portion
