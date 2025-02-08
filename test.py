c5b063c4236dc0bfd57e3ed7b6b767a11e9a058a
Dionathan <adiel8@gmail.com>
2025-02-07 19:44:06 -0700

    aws update

--- test.py
+++
@@ -1,8 +0,0 @@
-import boto3
-session = boto3.Session()
-credentials = session.get_credentials()
-
-if credentials is None:
-    print("No AWS credentials found!")
-else:
-    print("AWS credentials found!")
